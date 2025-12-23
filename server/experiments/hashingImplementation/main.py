9efdd695d4f6e50948dc79de5cde6e3532a73398
hariThorve <hariprasadthorve@gmail.com>
2025-12-23 19:27:49 +0530

    created post api to store user data in db with hash pass

--- server/experiments/hashingImplementation/main.py
+++ server/experiments/hashingImplementation/main.py
@@ -2,11 +2,24 @@
 import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from schema import create_db_and_table, Person, sessionDependency
+from contextlib import asynccontextmanager
+from hashing import bcryptPass
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_table()
+    yield
 
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
 
 @app.post("/sign_up")
-def signup(username : str, password : str):
-    pass
+def signup(person : Person, session : sessionDependency) -> Person:
+    person.password = bcryptPass.hash_password(person.password)
+    session.add(person)
+    session.commit()
+    session.refresh(person)
+    return person
