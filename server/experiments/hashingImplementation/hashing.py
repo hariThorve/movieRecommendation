efae3d9e53c53ac49361e9eb01ba12cd3d77221f
hariThorve <hariprasadthorve@gmail.com>
2025-12-23 19:26:56 +0530

    main hasing logic to convert password to hash

--- server/experiments/hashingImplementation/hashing.py
+++ server/experiments/hashingImplementation/hashing.py
@@ -1,20 +1,25 @@
 import bcrypt
 
-def hash_password(password : str):
-    passwordBytes = password.encode("utf-8")
-    # Generate a salt and hash the password in one step
-    hashedPass = bcrypt.hashpw(passwordBytes, bcrypt.gensalt())
-    return hashedPass.decode("utf-8")
+class bcryptPass: 
+    def hash_password(password : str):
+        passwordBytes = password.encode("utf-8")
+        # Generate a salt and hash the password in one step
+        hashedPass = bcrypt.hashpw(passwordBytes, bcrypt.gensalt())
+        return hashedPass.decode("utf-8")
 
-def validatePass(password : str, hashedPassword : str):
-    # convert inputs to bytes
-    passBytes = password.encode('utf-8')
-    storedHashBytes = hashedPassword.encode('utf-8')
+    def validatePass(password : str, hashedPassword : str):
+        # convert inputs to bytes
+        passBytes = password.encode('utf-8')
+        # print(f"converted input pass: {passBytes}")
+        storedHashBytes = hashedPassword.encode('utf-8')
+        # print(f"converted stored hash bytes: {storedHashBytes}")
 
-    return bcrypt.checkpw(passBytes, storedHashBytes)
+        return bcrypt.checkpw(passBytes, storedHashBytes)
 
-password = "harry123"
+# example use case
 
-hp = hash_password(password=password)
-print(hp)
-print(validatePass(password=password, hashedPassword=hp))
+# password = "harry123"
+
+# hp = hash_password(password=password)
+# print(hp)
+# print(validatePass(password=password, hashedPassword=hp))
