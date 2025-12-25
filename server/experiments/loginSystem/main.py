fb4af560d527114dea4be246534babb7c65952bb
hariThorve <hariprasadthorve@gmail.com>
2025-12-25 17:55:34 +0530

    sign-up endpoint created

--- server/experiments/loginSystem/main.py
+++ server/experiments/loginSystem/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from model import create_db_and_table, Student, sessionDependency
 from contextlib import asynccontextmanager
+from hashing import Bcrypt
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,3 +12,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+@app.post("/sign-up")
+def signup(student: Student, session: sessionDependency) -> Student:
+    student.password = Bcrypt.hash_password(student.password)
+    session.add(student)
+    session.commit()
+    session.refresh(student)
+    return student
